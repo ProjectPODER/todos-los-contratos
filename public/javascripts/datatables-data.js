28dd64d6c4ce6419ce5416a39b9643d73464ad9d
MC <marisol@rindecuentas.org>
2018-09-03 11:19:40 -0500

    Arreglo de estilos en tablas

--- public/javascripts/datatables-data.js
+++ public/javascripts/datatables-data.js
@@ -68,6 +68,94 @@ $(function(){
         { "data": "cirteria_score.total_score" }
     ]
   });
+
+  $('#tableStates').DataTable( {
+    "ajax": {
+      "url": "http://localhost:3000/api-data",
+      "dataSrc": ""
+    },
+    "pageLength": 10,
+    "paging": false,
+    "searching": false,
+    "info": false,
+    "columnDefs": [
+      { "orderable": false, "targets": 0 },
+      { "orderable": false, "targets": 1 },
+      { "orderable": false, "targets": 2 },
+      { "orderable": false, "targets": 3 },
+      { "orderable": false, "targets": 4 },
+      { "orderable": false, "targets": 5 },
+      { "orderable": false, "targets": 6 }
+    ],
+    "columns": [
+        { "data": "party.ranking" },
+        { "data": "party.name" },
+        { "data": "cirteria_score.transparencia" },
+        { "data": "cirteria_score.temporalidad" },
+        { "data": "cirteria_score.competitividad" },
+        { "data": "cirteria_score.trazabilidad" },
+        { "data": "cirteria_score.total_score" }
+    ]
+  });
+  $('#tableTopMunicipios').DataTable( {
+    "ajax": {
+      "url": "http://localhost:3000/api-data",
+      "dataSrc": ""
+    },
+    "pageLength": 5,
+    "paging": false,
+    "searching": false,
+    "info": false,
+    "columnDefs": [
+      { "orderable": false, "width": "130px", "targets": 0 },
+      { "orderable": false, "width": "138px", "targets": 1 },
+      { "orderable": false, "width": "138px", "targets": 2 },
+      { "orderable": false, "width": "138px", "targets": 3 },
+      { "orderable": false, "width": "140px", "targets": 4 },
+      { "orderable": false, "width": "140px", "targets": 5 },
+      { "orderable": false, "width": "140px", "targets": 6 }
+    ],
+    "order": [[ 0, 'asc' ]],
+    "columns": [
+        { "data": "party.ranking" },
+        { "data": "party.name" },
+        { "data": "cirteria_score.transparencia" },
+        { "data": "cirteria_score.temporalidad" },
+        { "data": "cirteria_score.competitividad" },
+        { "data": "cirteria_score.trazabilidad" },
+        { "data": "cirteria_score.total_score" }
+    ]
+  });
+
+  $('#tableBottomMunicipios').DataTable( {
+    "ajax": {
+      "url": "http://localhost:3000/api-data",
+      "dataSrc": ""
+    },
+    "pageLength": 5,
+    "paging": false,
+    "searching": false,
+    "info": false,
+    "columnDefs": [
+      { "orderable": false, "width": "130px", "targets": 0 },
+      { "orderable": false, "width": "138px", "targets": 1 },
+      { "orderable": false, "width": "138px", "targets": 2 },
+      { "orderable": false, "width": "138px", "targets": 3 },
+      { "orderable": false, "width": "140px", "targets": 4 },
+      { "orderable": false, "width": "140px", "targets": 5 },
+      { "orderable": false, "width": "140px", "targets": 6 }
+    ],
+    "order": [[ 0, 'desc' ]],
+    "columns": [
+        { "data": "party.ranking" },
+        { "data": "party.name" },
+        { "data": "cirteria_score.transparencia" },
+        { "data": "cirteria_score.temporalidad" },
+        { "data": "cirteria_score.competitividad" },
+        { "data": "cirteria_score.trazabilidad" },
+        { "data": "cirteria_score.total_score" }
+    ]
+  });
 });
 
     
